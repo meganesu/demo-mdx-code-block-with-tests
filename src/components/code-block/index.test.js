3b641107414dc36aed0bc3d40c8c887c45b66454
Megan Sullivan <meganesulli@gmail.com>
2023-01-23 20:17:01 -0800

    chore: set up Jest

---
+++ src/components/code-block/index.test.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import CodeBlock from "."
+
+describe('CodeBlock component', () => {
+  it('renders', () => {
+    expect(true).toEqual(true)
+  });
+
+  it('renders with file title', () => {
+    
+  });
+
+  it('renders with language tag', () => {
+
+  });
+});
