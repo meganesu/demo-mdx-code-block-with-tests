108dfad8ba3749edad65413f050b44ad2d24f7ba
Megan Sullivan <meganesulli@gmail.com>
2023-01-23 22:58:09 -0800

    wip: use @mdx-js/mdx evaluate() in CodeBlock test (throwing errors)

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
+  transformIgnorePatterns: [`(?!(node_modules))`],
+  // transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link|@mdx-js/mdx|unified|bail|is-plain-obj|trough|vfile|vfile-message|unist-util-stringify-position|remark-mdx|micromark-extension-mdxjs|micromark-util-combine-extensions|micromark-util-chunked|micromark-extension-mdx-expression|micromark-factory-mdx-expression|micromark-util-character|micromark-factory-space|unist-util-position-from-estree|micromark-util-events-to-acorn|estree-util-visit|vfile-location|micromark-extension-mdx-jsx|estree-util-is-identifier-name|micromark-extension-mdx-md|micromark-extension-mdxjs-esm|micromark-core-commonmark|micromark-util-classify-character|micromark-util-resolve-all|decode-named-character-reference|micromark-util-subtokenize|micromark-factory-destination|micromark-factory-label|micromark-factory-title|micromark-factory-whitespace|micromark-util-normalize-identifier|micromark-util-html-tag-name|mdast-util-mdx|mdast-util-mdx-expression|mdast-util-mdx-jsx|ccount|parse-entities|character-entities-legacy|character-reference-invalid|is-decimal|is-hexadecimal|is-alphanumerical|is-alphabetical|stringify-entities|character-entities-html4|mdast-util-to-markdown|mdast-util-mdxjs-esm|remark-parse|mdast-util-from-markdown|mdast-util-to-string)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
--- src/components/code-block/index.test.js
+++ src/components/code-block/index.test.js
@@ -1,26 +1,30 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 
+import * as runtime from 'react/jsx-runtime'
+import { evaluate } from "@mdx-js/mdx"
+
 import CodeBlock from "."
 
 describe('CodeBlock component', () => {
-  it('renders', () => {
-    const Title = () => (
-      <h1>Hi there!</h1>
-    )
-    
-    render(<Title />)
+  it('renders with file title', async () => {
+    const mdFencedCodeBlock = `
+      \`\`\`js title="my-test-file.js"
+      const test = "hey there!"
+      \`\`\`
+    `
 
-    const element = screen.getByText("Hi there!")
+    const MdxContent = await evaluate(
+      mdFencedCodeBlock,
+      { ...runtime, development: false },
+    ).default
+
+    render(
+      <MdxContent components={{pre: CodeBlock}} />
+    )
+
+    const element = screen.getByText("my-test-file.js")
 
     expect(element).toBeDefined()
-  });
-
-  it('renders with file title', () => {
-    
-  });
-
-  it('renders with language tag', () => {
-
   });
 });
