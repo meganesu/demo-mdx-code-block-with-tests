d682d0ee201db729db1e72916f88ba546f98565b
Megan Sullivan <meganesulli@gmail.com>
2023-01-23 20:24:02 -0800

    chore: set up React Testing Library

--- src/components/code-block/index.test.js
+++ src/components/code-block/index.test.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { render, screen } from "@testing-library/react"
 
 import CodeBlock from "."
 
 describe('CodeBlock component', () => {
   it('renders', () => {
-    expect(true).toEqual(true)
+    const Title = () => (
+      <h1>Hi there!</h1>
+    )
+    
+    render(<Title />)
+
+    const element = screen.getByText("Hi there!")
+
+    expect(element).toBeDefined()
   });
 
   it('renders with file title', () => {
